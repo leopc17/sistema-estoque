c71265c935a97ad55d40ac73786ff646d22f7105
senecabrito <limaseneca@gmail.com>
2025-05-31 14:42:45 -0300

    criação de arquivos de configuração

---
+++ src/resources/database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS mapdb;
+
+USE mapdb;
+
+CREATE TABLE IF NOT EXISTS produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    preco DOUBLE,
+    descricao TEXT
+);
