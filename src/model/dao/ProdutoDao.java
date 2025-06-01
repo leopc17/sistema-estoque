828e2064ff4bc34d765fd181cb1bf3c71727c525
leopc17 <LeonardoEAF45@gmail.com>
2025-06-01 20:22:19 -0300

    adicionado lançamento de exceção na assinatura do método

--- src/model/dao/ProdutoDao.java
+++ src/model/dao/ProdutoDao.java
@@ -2,13 +2,14 @@ package model.dao;
 
 import model.entities.Produto;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface ProdutoDao {
 
-    void insert(Produto p);
-    void update(Produto p);
-    void delete(int ID);
-    Produto findById(int ID);
-    List<Produto> findAll();
+    void insert(Produto p) throws SQLException;
+    void update(Produto p) throws SQLException;
+    void delete(int ID) throws SQLException;
+    Produto findById(int ID) throws SQLException;
+    List<Produto> findAll() throws SQLException;
 }
