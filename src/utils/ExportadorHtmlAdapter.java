20f897356526cbb8a4ea42665e391accdd2a6192
florido-hub <floridodinizz@gmail.com>
2025-06-02 11:05:43 -0300

    estilização da tabela html

--- src/utils/ExportadorHtmlAdapter.java
+++ src/utils/ExportadorHtmlAdapter.java
@@ -11,7 +11,7 @@ public class ExportadorHtmlAdapter implements ExportadorProdutos {
     @Override
     public void exportar(List<Produto> produtos, String nomeArquivo) {
         try (FileWriter writer = new FileWriter(nomeArquivo + ".html")) {
-            writer.append("<!DOCTYPE html>\n<html>\n<head><title>Produtos</title></head>\n<body>\n");
+            writer.append("<!DOCTYPE html>\n<html>\n<head><title>Produtos</title><style>table{border-collapse:collapse;}th,td{border:1px solid black;padding:8px;}</style></head>\n<body>\n");
             writer.append("  <h1>Lista de Produtos</h1>\n");
             writer.append("  <table border='1'>\n");
             writer.append("    <tr><th>ID</th><th>Nome</th><th>Descrição</th><th>Preço</th><th>Quantidade</th></tr>\n");
