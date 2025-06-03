b524ba11b31aff76a7841583144c43016410ed4f
leopc17 <LeonardoEAF45@gmail.com>
2025-06-03 15:09:01 -0300

    instanciação da classe Menu no método main

--- src/app/Main.java
+++ src/app/Main.java
@@ -8,9 +8,12 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) {
         Connection conn = null;
+        Menu menu = null;
 
         try {
             conn = DBConnection.getConnection();
+            menu = new Menu(conn);
+            menu.exibirMenu();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         } finally {
